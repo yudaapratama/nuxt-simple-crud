3a21ff06cbac84faab6387c4a0c0a4c22a4dbf5f
Yudha Pratama <yudhaprathamaa@gmail.com>
2024-02-29 22:57:52 +0700

    feat: add toggle compnent

---
+++ components/App/Toggle.vue
@@ -0,0 +1,22 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+</script>
+
+<template>
+  <UTooltip text="Toggle theme" :ui="{ popper: { strategy: 'absolute' } }">
+    <UToggle
+      v-model="isDark"
+      on-icon="i-mynaui-sun"
+      off-icon="i-mynaui-moon"
+    />
+  </UTooltip>
+</template>
