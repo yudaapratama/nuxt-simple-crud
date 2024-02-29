fd13ee7a069ad1ff33efac7b86c8315f6136702a
Yudha Pratama <yudhaprathamaa@gmail.com>
2024-02-29 22:56:26 +0700

    feat: add some modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,32 @@ export default defineNuxtConfig({
   typescript: {
     strict: true,
     typeCheck: true
+  },
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/google-fonts',
+    '@nuxt/image',
+    '@pinia/nuxt'
+  ],
+  ui: {
+    icons: ['heroicons', 'mynaui']
+  },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+        class: 'h-full'
+      },
+      bodyAttrs: {
+        class: 'antialiased bg-white-50 dark:bg-slate-800 min-h-screen'
+      }
+    }
+  },
+  googleFonts: {
+    display: 'swap',
+    families: {
+      Nunito: [400, 500, 600, 700, 800, 900]
+    }
   }
 })
