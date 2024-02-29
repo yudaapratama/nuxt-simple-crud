91af213d1be7c6cc4c0821693bdda6be91a487a2
Yudha Pratama <yudhaprathamaa@gmail.com>
2024-02-29 22:55:56 +0700

    feat: add navbar and pages

--- app.vue
+++ app.vue
@@ -1,5 +1,52 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtLoadingIndicator color="#14b8a6" /> -->
+    <AppNavbar />
+    <UContainer class="py-12">
+      <NuxtPage />
+    </UContainer>
   </div>
+  <!-- <AppFooter /> -->
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.2s;
+}
+
+.page-leave-to {
+  opacity: 0;
+  transform: translateY(-5px);
+}
+
+.page-enter-from {
+  opacity: 0;
+  transform: translateY(5px);
+}
+
+.dark {
+--color-primary-DEFAULT: var(--color-primary-400);
+}
+
+/* width */
+::-webkit-scrollbar {
+    width: 8px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 6px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: #555;
+}
+</style>
