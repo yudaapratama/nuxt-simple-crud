8da8a0c6dd2012dc90f6b3c597419030f80ed20d
Yudha Pratama <yudhaprathamaa@gmail.com>
2024-02-29 22:55:21 +0700

    style: add default theme

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    primary: 'sky',
+    gray: 'slate',
+    button: {
+      rounded:
+        'rounded-md transition-transform active:scale-x-[0.98] active:scale-y-[0.99]'
+    },
+    container: {
+      constrained: 'w-full'
+    }
+  }
+})
